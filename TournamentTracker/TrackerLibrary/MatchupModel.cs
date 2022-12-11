5d0166c874c1f6432b8f4e6502c338c387e1f33e
William Nkosi <william.dev.acc@gmail.com>
2022-12-11 16:04:18 -0500

    Created Models library

---
+++ TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        /// <summary>
+        /// Represents one team in the matchup 
+        /// </summary>
+        public List<MatchUpEntryModel> entries { get; set; } = new List<MatchUpEntryModel>();
+
+        /// <summary>
+        /// Respresents the score for this particular team
+        /// </summary>
+        public TeamModel Winner { get; set; }
+
+        public int MatchRound { get; set; }
+           
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialScore"></param>
+        public MatchupModel(double initialScore)
+        {
+
+        }
+    }
+}
